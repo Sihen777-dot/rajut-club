05aeffd3142fad399a625eafe560f018fe881f88
hens <hensalosa727@gmail.com>
2026-09-25 09:32:00 +0700

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,7 +8,7 @@ const artikelModel = require("../models/artikelModel");
 const pembelianModel = require("../models/pembelianModel");
 
 // Kategori sesuai ENUM kolom produk.kategori di database
-const KATEGORI_VALID = ["Pakaian", "Aksesoris", "Tas&Mainan"];
+const KATEGORI_VALID = ["Pakaian", "Aksesoris", "Tas & Mainan"];
 
 // ===================== CRUD PRODUK =====================
 
